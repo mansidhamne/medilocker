1d1c0b95423181776abe9839e4efedc2167fe2a6
mansayy <mansiadhamne@gmail.com>
2024-10-24 23:56:31 +0530

    pre-pre-pre-final-not-not-not stage

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,28 +1,8 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   const corsOptions: CorsOptions = {
-//     origin: 'http://localhost:3001', // Replace with your frontend URL
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//   };
-
-//   app.enableCors(corsOptions);
-
-//   await app.listen(3000);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
-async function bootstrap(req: VercelRequest, res: VercelResponse) {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
@@ -31,15 +11,35 @@ async function bootstrap(req: VercelRequest, res: VercelResponse) {
     credentials: true,
   };
 
-  // Enable CORS with the specified options
   app.enableCors(corsOptions);
 
-  // Initialize the Nest application without listening on a port
-  await app.init();
-
-  // Call the NestJS request handler for the serverless function
-  await app.getHttpAdapter().getInstance()(req, res);
+  await app.listen(3000);
 }
+bootstrap();
 
-// Export the bootstrap function as default
-export default bootstrap;
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
+
+// async function bootstrap(req: VercelRequest, res: VercelResponse) {
+//   const app = await NestFactory.create(AppModule);
+
+//   const corsOptions: CorsOptions = {
+//     origin: 'http://localhost:3001', // Replace with your frontend URL
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   };
+
+//   // Enable CORS with the specified options
+//   app.enableCors(corsOptions);
+
+//   // Initialize the Nest application without listening on a port
+//   await app.init();
+
+//   // Call the NestJS request handler for the serverless function
+//   await app.getHttpAdapter().getInstance()(req, res);
+// }
+
+// // Export the bootstrap function as default
+// export default bootstrap;

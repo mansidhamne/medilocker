5a2cb234f4db9581fc68c42e2c7e68dfeeca5e23
mansayy <mansiadhamne@gmail.com>
2024-10-24 23:36:39 +0530

    pre-pre-pre-final-not stage

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+// //import { AuthGuard } from '@nestjs/passport';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+//   // @UseGuards(AuthGuard('google'))
+//   // async googleAuth(@Req() req) {}
+
+//   // @Get('auth/google/callback')
+//   // @UseGuards(AuthGuard('google'))
+//   // async googleAuthRedirect(@Req() req) {
+//   //   return this.appService.googleLogin(req);
+//   // }
+// }
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-  @UseGuards(AuthGuard('google'))
-  async googleAuth(@Req() req) {}
-
-  @Get('auth/google/callback')
-  @UseGuards(AuthGuard('google'))
-  async googleAuthRedirect(@Req() req) {
-    return this.appService.googleLogin(req);
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,14 +1,26 @@
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class AppService {
+//   // googleLogin(req) {
+//   //   if (!req.user) {
+//   //     return 'No user from google';
+//   //   }
+//   //   return {
+//   //     message: 'User information from google',
+//   //     user: req.user,
+//   //   };
+//   // }
+//   async getHello() {
+//     return 'Hello World!';
+//   }
+// }
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  googleLogin(req) {
-    if (!req.user) {
-      return 'No user from google';
-    }
-    return {
-      message: 'User information from google',
-      user: req.user,
-    };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
